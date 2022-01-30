1c379aac8a79f17f8461c99a558818ac75e0891c
sofiaoreis <sofiareis1994@gmail.com>
2022-01-30 02:28:38 +0000

    scripts to generate nvd data

--- tools/nvd/generate_data.sh
+++ tools/nvd/generate_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PYTHON=python3
-DATA_FOLDER=data/
+DATA_FOLDER=../../data/nvd/year/
 OUTPUT_FOLDER=../../data/nvd/
 RAW_DATA_FILENAME=raw-nvd-data.csv
 PATCHES_DATA_FILENAME=all-nvd-patches.csv
@@ -8,8 +8,21 @@ PATCHES_DATA_FILENAME=all-nvd-patches.csv
 echo "Extract and merge data from NVD files..."
 $PYTHON cli.py --task=extractor --data=$DATA_FOLDER --fout=$OUTPUT_FOLDER
 
-echo "Process NVD data..."
-$PYTHON cli.py --task=process --fin=$OUTPUT_FOLDER/$RAW_DATA_FILENAME \
+echo "\nProcess references ..."
+$PYTHON ../common/references.py --task=process \
+                                --fin=$OUTPUT_FOLDER/$RAW_DATA_FILENAME \
+                                --fout=$OUTPUT_FOLDER/$RAW_DATA_FILENAME
+
+echo "\nGetting the commits references to github, bitbucket, gitlab and git..."
+$PYTHON ../common/references.py --task=commits \
+                                --fin=$OUTPUT_FOLDER/$RAW_DATA_FILENAME \
                                 --fout=$OUTPUT_FOLDER/$PATCHES_DATA_FILENAME
 
-                     
+echo "\nGenerating data statistics..."
+$PYTHON ../common/references.py --task=stats  \
+                                --fin=$OUTPUT_FOLDER/$PATCHES_DATA_FILENAME
+
+echo "\nCleaning the data..."
+$PYTHON ../common/references.py --task=normalize \
+                                --fin=$OUTPUT_FOLDER/$PATCHES_DATA_FILENAME 
+      
